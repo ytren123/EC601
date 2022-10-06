a7a0f0b903f024e791b056e5f7b43e1d59b821e5
ytren123 <113720449+ytren123@users.noreply.github.com>
2022-10-06 15:16:33 -0400

    Add files via upload

---
+++ twitter_api/NLP_api_test.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+from google.cloud import language_v1
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './test-proj-364717-4b99109b6f3d.json'
+client = language_v1.LanguageServiceClient()
+
+text = "Nice weather!"
+document = language_v1.Document(
+    content=text, type_=language_v1.Document.Type.PLAIN_TEXT
+)
+
+sentiment = client.analyze_sentiment(
+    request={"document": document}
+).document_sentiment
+
+print("Text: {}".format(text))
+print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
