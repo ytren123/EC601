b40d18737749495ceb820fb5bc1eaa5cd0688c3b
fengyilei <113853377+fengyilei@users.noreply.github.com>
2022-11-15 13:55:02 -0500

    Update tiwitterAPI_1.py

--- twitter_api/tiwitterAPI_1.py
+++ twitter_api/tiwitterAPI_1.py
@@ -16,4 +16,52 @@ def get_all_tweets(screen_name):
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     alltweets= []
-    new_tweets = api.user_timeline(screen_name=screen_name, count=20, )
+    new_tweets = api.user_timeline(screen_name=screen_name, count=20,  include_rst = False, exclude_replies = True)
+      alltweets.extend(new_tweets)
+    
+    
+    oldest = alltweets[-1].id - 1
+    
+    
+    while len(new_tweets) > 0:
+        new_tweets = api.user_timeline(screen_name = screen_name, count = 20, max_id = oldest, include_rst = False, exclude_replies = True)
+        
+        alltweets.extend(new_tweets)
+        
+        oldest = alltweets[-1].id - 1
+        if(len(alltweets) > 15):
+            break
+        print("...%s tweets downloaded so far" % (len(alltweets)))
+        
+    return alltweets
+
+#I use this code to create a directory
+def imagedir(downloaded_images):
+    #I need to create a folder for all of my images
+    path = os.getcwd() + "/" + downloaded_images
+    
+    if not(os.path.isdir(path)):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print('Directory %s will not be created' % path)
+        else:
+            shutill.tmtree(path)
+            print('Directory %s will be created' % path)
+    return path
+
+if __name__ == '__main__':
+#I use this code for download images from my twitter account
+    path = imagedir('images')
+    
+    tweets = get_all_tweets('@zwang_z')
+    
+    #get the url from twitter data
+    image_url = set()
+    for post in tweets:
+        media = post.entities.get('media',[])
+        if(len(media) > 0):
+            image_url.add(media[0]['media_url'])
+            
+    for i, url in enumerate(image_url):
+        urllib.request.urlretrieve(url, path+'/'+str(i)+'.jpg')
