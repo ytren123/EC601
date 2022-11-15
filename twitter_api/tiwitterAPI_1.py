0a5cc7aee3aa126eeb4f145fc9c95e0c2363ee84
fengyilei <113853377+fengyilei@users.noreply.github.com>
2022-11-15 13:50:19 -0500

    Create tiwitterAPI_1.py

---
+++ twitter_api/tiwitterAPI_1.py
@@ -0,0 +1,17 @@
+import tweepy
+import os
+import sys
+import subprocess
+import urllib.request
+from subprocess import call
+
+#These are my Twitter API key
+consumer_key = "qIC45ymdG2qll74ExHuuWGhUM"
+consumer_secret = "sNbTuogmPfH5Rg9t51c13D8SrZG8KOT5txNp6Nrs8lj9A4kPDz"
+access_token = "1039188583154900992-smY4pmpPFSy2DWWyIGYGa6PhqBzmRP"
+access_token_secret = "oeDkqE4tp3Xa1lKqaVJYbNuTlKhkHlRtSsJYpfaxn3Bff"
+
+def get_all_tweets(screen_name):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
